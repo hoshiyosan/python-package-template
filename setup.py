d372478d8f783910fcb6e6e77158e9acd6a37a63
Sylvan Le Deunff <sylvan.ledeunff@orange.com>
2020-11-09 14:25:53 +0100

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def list_requirements():
 setuptools.setup(
     name='example',
     description='example package structure',
-    version='0.0.1',
+    version='0.0.2',
     packages=setuptools.find_packages(),
     install_requires=list_requirements()
 )
