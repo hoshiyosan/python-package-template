59e0b6faf7a061c55d88231ed598a4a2c1043fef
Sylvan Le Deunff <sylvan.ledeunff@orange.com>
2020-11-09 19:03:36 +0100

    "[DevOps] Update package version 0.0.3 → 0.0.4"

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def list_requirements():
 setuptools.setup(
     name='example',
     description='example package structure',
-    version='0.0.3',
+    version='0.0.4',
     packages=setuptools.find_packages(),
     install_requires=list_requirements()
 )
