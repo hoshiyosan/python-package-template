688ceff2c09641e3dc076ee45a38f55d2d11a7c2
Sylvan Le Deunff <sylvan.ledeunff@orange.com>
2020-11-09 19:15:46 +0100

    [DevOps] Update package version 0.0.6 → 0.0.7

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def list_requirements():
 setuptools.setup(
     name='example',
     description='example package structure',
-    version='0.0.6',
+    version='0.0.7',
     packages=setuptools.find_packages(),
     install_requires=list_requirements()
 )
